9dc8ff6c55481de06a3742b76f8b820f457213e5
saurav004 <logtosaurav@gmail.com>
2020-10-20 21:59:55 +0530

    Added:employee attendance is checked

--- src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
@@ -3,7 +3,14 @@ package com.bridgelabz.java.EmployeeWage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("-------------------------Welcome to Employee Wage Computation Problem-------------------------");
+		System.out.println(
+				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
+		int IS_FULL_TIME = 1;
+		int attendance = (int) (Math.round(Math.random()));
+		if (attendance == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
