75041088dc32cc2207b484911d10b4fd57d3a78d
saurav004 <logtosaurav@gmail.com>
2020-10-20 21:26:49 +0530

    Added:package com/bridgelabz/java/EmployeeWage/ , contains Welcome message

---
+++ src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.java.EmployeeWage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("-------------------------Welcome to Employee Wage Computation Problem-------------------------");
+	}
+
+}
