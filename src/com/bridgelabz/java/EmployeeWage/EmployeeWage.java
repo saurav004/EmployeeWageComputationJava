00fde486546af6d3287f15ac1e6983203ed64f72
saurav004 <logtosaurav@gmail.com>
2020-10-27 02:11:22 +0530

    Added:code to calculate wage of an Employee in different companies

--- src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
@@ -1,19 +1,39 @@
 package com.bridgelabz.java.EmployeeWage;
 
-public class EmployeeWage {
+import java.util.Scanner;
 
-	static int workingDaysInMonth = 20;
-	static int workingHoursinMonth = 100;
-	static int empWagePerHour = 20;
+public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println(
 				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
-		int totalMonthlyWage = empWageForMonth();
-		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
+		Scanner sc = new Scanner(System.in);
+		String companyName = null;
+
+		int exit = 0;
+		while (exit == 0) {
+			System.out.println("Enter your choice");
+			System.out.println("1.\t EXIT\t 2.\t calculate Employee Wage");
+			int choice1 = sc.nextInt();
+			if (choice1 == 1) {
+				exit = 1;
+			} else {
+				System.out.println("Enter company name");
+				companyName = sc.next();
+				System.out.println("Enter Working days in a month");
+				int workingDaysInMonth = sc.nextInt();
+				System.out.println("Enter Working hours in a month");
+				int workingHoursinMonth = sc.nextInt();
+				System.out.println("Enter emp wage per hour");
+				int empWagePerHour = sc.nextInt();
+				empWageForMonth(companyName, workingDaysInMonth, workingHoursinMonth, empWagePerHour);
+			}
+		}
+		sc.close();
 	}
 
-	public static int empWageForMonth() {
+	public static void empWageForMonth(String CompanyName, int workingDaysInMonth, int workingHoursinMonth,
+			int empWagePerHour) {
 		int end = 0;
 		int empHrs = 0;
 		int empDailyWage = 0;
@@ -23,12 +43,12 @@ public class EmployeeWage {
 		while (end >= 0) {
 			++totalEmpDays;
 			int attendance = (int) (Math.floor(Math.random() * 10) % 3);
-			System.out.println(attendance);
+			System.out.println("\n" + attendance);
 			switch (attendance) {
 			case 0:
+				empHrs = 0;
 				System.out.println("Employee is absent");
 				System.out.println();
-				empHrs = 0;
 				break;
 			case 1:
 				System.out.println("Employee is present");
@@ -43,13 +63,17 @@ public class EmployeeWage {
 			}
 			totalHrs += empHrs;
 			empDailyWage = empWagePerHour * empHrs;
-			System.out.println("Employee's salary for the day is " + empDailyWage);
+			System.out.println("\nEmployee's salary for the day is " + empDailyWage);
 			totalMonthlyWage += empDailyWage;
 			if (totalEmpDays == workingDaysInMonth || totalHrs == workingHoursinMonth) {
 				break;
 			}
 		}
 		System.out.println("Total Employee Hours: " + totalHrs + " total Employee days " + totalEmpDays);
-		return totalMonthlyWage;
+		System.out.println();
+		System.out.println("company name\t" + CompanyName);
+		System.out.println();
+		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
+		System.out.println();
 	}
 }
