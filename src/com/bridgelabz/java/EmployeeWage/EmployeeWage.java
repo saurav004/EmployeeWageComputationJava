ad16f03f03173d2fd3965dbb47c76118d29a0a4b
saurav004 <logtosaurav@gmail.com>
2020-10-27 07:39:27 +0530

    Added:saving Employee Wage for different companies

--- src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/java/EmployeeWage/EmployeeWage.java
@@ -1,39 +1,60 @@
 package com.bridgelabz.java.EmployeeWage;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeWage {
+
+	String CompanyName;
+	int empWagePerHour;
+	int workingDaysInMonth;
+	int workingHoursinMonth;
+	int totalMonthlyWage;
+
+	public EmployeeWage(String CompanyName, int empWagePerHour, int workingHoursinMonth, int workingDaysInMonth) {
+		this.CompanyName = CompanyName;
+		this.empWagePerHour = empWagePerHour;
+		this.workingDaysInMonth = workingDaysInMonth;
+		this.workingHoursinMonth = workingHoursinMonth;
+		this.totalMonthlyWage = empWageForMonth(this.CompanyName, this.empWagePerHour, this.workingHoursinMonth,
+				this.workingDaysInMonth);
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println(
 				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
 		Scanner sc = new Scanner(System.in);
 		String companyName = null;
-
-		int exit = 0;
-		while (exit == 0) {
-			System.out.println("Enter your choice");
-			System.out.println("1.\t EXIT\t 2.\t calculate Employee Wage");
-			int choice1 = sc.nextInt();
-			if (choice1 == 1) {
-				exit = 1;
-			} else {
-				System.out.println("Enter company name");
-				companyName = sc.next();
-				System.out.println("Enter Working days in a month");
-				int workingDaysInMonth = sc.nextInt();
-				System.out.println("Enter Working hours in a month");
-				int workingHoursinMonth = sc.nextInt();
-				System.out.println("Enter emp wage per hour");
-				int empWagePerHour = sc.nextInt();
-				empWageForMonth(companyName, workingDaysInMonth, workingHoursinMonth, empWagePerHour);
-			}
+		ArrayList<Object> saveCompanyWiseWage = new ArrayList<Object>();
+		int numOfObjects = 0;
+		System.out.println("Enter how many objects you want to add");
+		numOfObjects = sc.nextInt();
+		for (int i = 0; i < numOfObjects; i++) {
+			System.out.println("Enter company name");
+			companyName = sc.next();
+			System.out.println("Enter Working days in a month");
+			int workingDaysInMonth = sc.nextInt();
+			System.out.println("Enter Working hours in a month");
+			int workingHoursinMonth = sc.nextInt();
+			System.out.println("Enter emp wage per hour");
+			int empWagePerHour = sc.nextInt();
+			EmployeeWage emp = new EmployeeWage(companyName, empWagePerHour, workingHoursinMonth, workingDaysInMonth);
+			saveCompanyWiseWage.add(emp);
+		}
+		for (Object emp : saveCompanyWiseWage) {
+			System.out.println(emp);
 		}
 		sc.close();
 	}
 
-	public static void empWageForMonth(String CompanyName, int workingDaysInMonth, int workingHoursinMonth,
-			int empWagePerHour) {
+	public String toString() {
+		return "employee Wage for the CompanyName:\t" + CompanyName + "\tis\t" + totalMonthlyWage;
+
+	}
+
+	public static int empWageForMonth(String CompanyName, int empWagePerHour, int workingHoursinMonth,
+			int workingDaysInMonth) {
 		int end = 0;
 		int empHrs = 0;
 		int empDailyWage = 0;
@@ -69,11 +90,6 @@ public class EmployeeWage {
 				break;
 			}
 		}
-		System.out.println("Total Employee Hours: " + totalHrs + " total Employee days " + totalEmpDays);
-		System.out.println();
-		System.out.println("company name\t" + CompanyName);
-		System.out.println();
-		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
-		System.out.println();
+		return totalMonthlyWage;
 	}
 }
